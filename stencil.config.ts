2ec8f249f5398cf539a733230261b4abfe7de0d4
Charlie Gleason <hi@charliegleason.com>
2019-11-26 12:45:39 +0000

    Fixes for local function

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,18 +1,24 @@
-import { Config } from '@stencil/core';
+import { Config } from '@stencil/core'
 
 export const config: Config = {
   namespace: 'shader-mapper',
+  commonjs: {
+    include: /node_modules|(..\/.+)/,
+    namedExports: {
+      'commonjs-dep': ['shader-mapper'],
+    },
+  } as any,
   outputTargets: [
     {
       type: 'dist',
-      esmLoaderPath: '../loader'
+      esmLoaderPath: '../loader',
     },
     {
-      type: 'docs-readme'
+      type: 'docs-readme',
     },
     {
       type: 'www',
-      serviceWorker: null // disable service workers
-    }
-  ]
-};
+      serviceWorker: null, // disable service workers
+    },
+  ],
+}
