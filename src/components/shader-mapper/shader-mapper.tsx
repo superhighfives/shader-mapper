4ebad8380a4ed8facb59b24693d8daa9565dbbc8
Charlie Gleason <hi@charliegleason.com>
2020-01-03 15:15:24 +0000

    Fixed up a bunch of stuff

--- src/components/shader-mapper/shader-mapper.tsx
+++ src/components/shader-mapper/shader-mapper.tsx
@@ -1,137 +1,157 @@
-import { Component, Element, Prop, Host, h } from '@stencil/core'
-import { Uniform, Rect } from '../../utils/utils'
+import { Component, Element, Listen, Prop, Host, h } from "@stencil/core";
+import { Uniform, EasingUniform, Rect } from "../../utils/utils";
 
 @Component({
-  tag: 'shader-mapper',
-  styleUrl: 'shader-mapper.css',
-  shadow: true,
+  tag: "shader-mapper",
+  styleUrl: "shader-mapper.css",
+  shadow: true
 })
 export class Mapper {
-  @Element() private element: HTMLElement
+  @Element() private element: HTMLElement;
 
-  @Prop() fragment: string
-  @Prop() vertex: string
-  @Prop() images: string
+  @Prop() fragment: string;
+  @Prop() vertex: string;
+  @Prop() images: string;
 
-  canvas: HTMLCanvasElement
-  context: any
+  @Listen("scroll", { target: "window" })
+  handleScroll(event) {
+    this.uThreshold &&
+      this.uThreshold.set(
+        event.currentTarget.scrollX,
+        event.currentTarget.scrollY
+      );
+  }
 
-  ratio: any
-  width: any
-  height: any
-  windowWidth: any
-  windowHeight: any
+  @Listen("turbulenceStart", { target: "window" })
+  turbulenceStartHandler() {
+    this.uMovement && this.uMovement.set(this.uMovement.state + 1);
+  }
 
-  program: any
-  textures: any
-  output: any
-  positionLocation: any
-  startTime: any
-  imagesAspect: any
+  @Listen("turbulenceStop", { target: "window" })
+  turbulenceStopHandler() {
+    this.uMovement && this.uMovement.set(0, { delay: 0.25 });
+  }
 
-  uTime: any
-  uResolution: any
-  uRatio: any
-  uThreshold: any
+  canvas: HTMLCanvasElement;
+  context: any;
 
-  loadedFragment: any
-  loadedVertex: any
-  loadedImages: any
+  ratio: any;
+  width: any;
+  height: any;
+  windowWidth: any;
+  windowHeight: any;
+
+  program: any;
+  textures: any;
+  output: any;
+  positionLocation: any;
+  startTime: any;
+  imagesAspect: any;
+
+  uTime: any;
+  uResolution: any;
+  uRatio: any;
+  uThreshold: any;
+  uMovement: any;
+
+  loadedFragment: any;
+  loadedVertex: any;
+  loadedImages: any;
 
   componentDidLoad() {
-    this.canvas = this.element.shadowRoot.querySelector('#canvas')
-    this.context = this.canvas.getContext('webgl')
-    this.ratio = window.devicePixelRatio
+    this.canvas = this.element.shadowRoot.querySelector("#canvas");
+    this.context = this.canvas.getContext("webgl");
+    this.ratio = window.devicePixelRatio;
 
-    this.context.getExtension('OES_standard_derivatives')
+    this.context.getExtension("OES_standard_derivatives");
 
-    this.startTime = new Date().getTime()
+    this.startTime = new Date().getTime();
 
-    const loaders = []
-    const images = this.images && this.images.split(',')
+    const loaders = [];
+    const images = this.images && this.images.split(",");
     const shaders = [
-      { name: 'fragment', url: this.fragment },
-      { name: 'vertex', url: this.vertex },
-    ]
-    const loadedShaders: any = {}
-    const loadedImages: any = []
+      { name: "fragment", url: this.fragment },
+      { name: "vertex", url: this.vertex }
+    ];
+    const loadedShaders: any = {};
+    const loadedImages: any = [];
 
     shaders.forEach(shader => {
       loaders.push(
         fetch(shader.url)
           .then(response => {
-            return response.text()
+            return response.text();
           })
           .then(result => {
-            loadedShaders[shader.name] = result
+            loadedShaders[shader.name] = result;
           })
-      )
-    })
+      );
+    });
 
     images &&
       images.forEach(url => {
         loaders.push(
           new Promise((resolve, reject) => {
-            const img = new Image()
-            img.addEventListener('load', () => {
-              loadedImages.push(img)
-              resolve()
-            })
-            img.addEventListener('error', () => {
-              reject(new Error(`Failed to load image's URL: ${url}`))
-            })
-            img.src = url
+            const img = new Image();
+            img.addEventListener("load", () => {
+              loadedImages.push(img);
+              resolve();
+            });
+            img.addEventListener("error", () => {
+              reject(new Error(`Failed to load image's URL: ${url}`));
+            });
+            img.src = url;
           })
-        )
-      })
+        );
+      });
 
     Promise.all(loaders).then(() => {
-      this.loadedFragment = loadedShaders.fragment
-      this.loadedVertex = loadedShaders.vertex
+      this.loadedFragment = loadedShaders.fragment;
+      this.loadedVertex = loadedShaders.vertex;
 
-      this.createScene()
+      this.createScene();
 
       if (loadedImages.length) {
-        this.loadedImages = loadedImages
-        this.createTextures()
+        this.loadedImages = loadedImages;
+        this.createTextures();
       }
 
-      this.setSizes()
-      this.loop()
-    })
+      this.setSizes();
+      this.loop();
+    });
   }
 
   createTextures() {
-    const images = this.loadedImages
-    this.imagesAspect = images[0].naturalHeight / images[0].naturalWidth
+    const images = this.loadedImages;
+    this.imagesAspect = images[0].naturalHeight / images[0].naturalWidth;
 
-    this.textures = []
+    this.textures = [];
 
     for (var i = 0; i < images.length; i++) {
-      let texture = this.context.createTexture()
-      this.context.bindTexture(this.context.TEXTURE_2D, texture)
+      let texture = this.context.createTexture();
+      this.context.bindTexture(this.context.TEXTURE_2D, texture);
 
       // Set the parameters so we can render any size image.
       this.context.texParameteri(
         this.context.TEXTURE_2D,
         this.context.TEXTURE_WRAP_S,
         this.context.CLAMP_TO_EDGE
-      )
+      );
       this.context.texParameteri(
         this.context.TEXTURE_2D,
         this.context.TEXTURE_WRAP_T,
         this.context.CLAMP_TO_EDGE
-      )
+      );
       this.context.texParameteri(
         this.context.TEXTURE_2D,
         this.context.TEXTURE_MIN_FILTER,
         this.context.LINEAR
-      )
+      );
       this.context.texParameteri(
         this.context.TEXTURE_2D,
         this.context.TEXTURE_MAG_FILTER,
         this.context.LINEAR
-      )
+      );
 
       // Upload the image into the texture.
       this.context.texImage2D(
@@ -141,54 +161,63 @@ export class Mapper {
         this.context.RGBA,
         this.context.UNSIGNED_BYTE,
         images[i]
-      )
-      this.textures.push(texture)
+      );
+      this.textures.push(texture);
 
       // lookup the sampler locations.
       const u_imageLocation = this.context.getUniformLocation(
         this.program,
         `u_image${i}`
-      )
+      );
 
       // set which texture units to render with.
-      this.context.uniform1i(u_imageLocation, i) // texture unit i
+      this.context.uniform1i(u_imageLocation, i); // texture unit i
 
       // Set each texture unit to use a particular texture.
-      this.context.activeTexture(this.context[`TEXTURE${i}`])
-      this.context.bindTexture(this.context.TEXTURE_2D, this.textures[i])
+      this.context.activeTexture(this.context[`TEXTURE${i}`]);
+      this.context.bindTexture(this.context.TEXTURE_2D, this.textures[i]);
     }
   }
 
   createScene() {
-    this.program = this.context.createProgram()
+    this.program = this.context.createProgram();
 
-    this.addShader(this.loadedVertex, this.context.VERTEX_SHADER)
-    this.addShader(this.loadedFragment, this.context.FRAGMENT_SHADER)
+    this.addShader(this.loadedVertex, this.context.VERTEX_SHADER);
 
-    this.context.linkProgram(this.program)
-    this.context.useProgram(this.program)
+    console.log(this.loadedFragment, this.context.FRAGMENT_SHADER);
+
+    this.addShader(this.loadedFragment, this.context.FRAGMENT_SHADER);
+
+    this.context.linkProgram(this.program);
+    this.context.useProgram(this.program);
 
     this.uResolution = new Uniform(
-      'u_resolution',
-      '4f',
+      "u_resolution",
+      "4f",
       this.program,
       this.context
-    )
-    this.uTime = new Uniform('u_time', '1f', this.program, this.context)
-    this.uRatio = new Uniform('u_pixelRatio', '1f', this.program, this.context)
+    );
+    this.uTime = new Uniform("u_time", "1f", this.program, this.context);
+    this.uRatio = new Uniform("u_pixelRatio", "1f", this.program, this.context);
     this.uThreshold = new Uniform(
-      'u_threshold',
-      '2f',
+      "u_threshold",
+      "2f",
       this.program,
       this.context
-    )
+    );
+    this.uMovement = new EasingUniform(
+      "u_movement",
+      "1f",
+      this.program,
+      this.context
+    );
 
-    this.output = new Rect(this.context)
+    this.output = new Rect(this.context);
     this.positionLocation = this.context.getAttribLocation(
       this.program,
-      'a_position'
-    )
-    this.context.enableVertexAttribArray(this.positionLocation)
+      "a_position"
+    );
+    this.context.enableVertexAttribArray(this.positionLocation);
     this.context.vertexAttribPointer(
       this.positionLocation,
       2,
@@ -196,67 +225,68 @@ export class Mapper {
       false,
       0,
       0
-    )
+    );
   }
 
   setSizes() {
-    this.windowWidth = window.innerWidth
-    this.windowHeight = window.innerHeight
-    this.width = this.element.offsetWidth
-    this.height = this.element.offsetHeight
+    this.windowWidth = window.innerWidth;
+    this.windowHeight = window.innerHeight;
+    this.width = this.element.offsetWidth;
+    this.height = this.element.offsetHeight;
 
-    this.canvas.width = this.width * this.ratio
-    this.canvas.height = this.height * this.ratio
+    this.canvas.width = this.width * this.ratio;
+    this.canvas.height = this.height * this.ratio;
 
-    let a1: any, a2: any
+    let a1: any, a2: any;
 
     if (this.imagesAspect) {
       if (this.height / this.width < this.imagesAspect) {
-        a1 = 1
-        a2 = this.height / this.width / this.imagesAspect
+        a1 = 1;
+        a2 = this.height / this.width / this.imagesAspect;
       } else {
-        a1 = (this.width / this.height) * this.imagesAspect
-        a2 = 1
+        a1 = (this.width / this.height) * this.imagesAspect;
+        a2 = 1;
       }
     } else {
-      a1 = 1
-      a2 = 1
+      a1 = 1;
+      a2 = 1;
     }
 
-    this.uResolution.set(this.width, this.height, a1, a2)
-    this.uRatio.set(1 / this.ratio)
-    this.uThreshold.set(0, 0)
+    this.uResolution.set(this.width, this.height, a1, a2);
+    this.uRatio.set(1 / this.ratio);
+    this.uThreshold.set(window.scrollX, window.scrollY);
+    this.uMovement.set(0);
     this.context.viewport(
       0,
       0,
       this.width * this.ratio,
       this.height * this.ratio
-    )
+    );
   }
 
   addShader(source: any, type: any) {
-    let shader = this.context.createShader(type)
-    this.context.shaderSource(shader, source)
-    this.context.compileShader(shader)
+    let shader = this.context.createShader(type);
+    this.context.shaderSource(shader, source);
+    this.context.compileShader(shader);
     let isCompiled = this.context.getShaderParameter(
       shader,
       this.context.COMPILE_STATUS
-    )
+    );
     if (!isCompiled) {
       throw new Error(
-        'Shader compile error: ' + this.context.getShaderInfoLog(shader)
-      )
+        "Shader compile error: " + this.context.getShaderInfoLog(shader)
+      );
     }
-    this.context.attachShader(this.program, shader)
+    this.context.attachShader(this.program, shader);
   }
 
   loop() {
-    let now = new Date().getTime()
-    let currentTime = (now - this.startTime) / 1000
-    this.uTime.set(currentTime)
+    let now = new Date().getTime();
+    let currentTime = (now - this.startTime) / 1000;
+    this.uTime.set(currentTime);
 
-    this.output.render(this.context)
-    requestAnimationFrame(this.loop.bind(this))
+    this.output.render(this.context);
+    requestAnimationFrame(this.loop.bind(this));
   }
 
   render() {
@@ -264,6 +294,6 @@ export class Mapper {
       <Host>
         <canvas id="canvas"></canvas>
       </Host>
-    )
+    );
   }
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,36 +1,72 @@
+import { gsap, Elastic } from "gsap";
+
 export class Uniform {
-  name: any
-  suffix: any
-  gl: any
-  program: any
-  location: any
+  name: any;
+  suffix: any;
+  gl: any;
+  program: any;
+  location: any;
 
   constructor(name, suffix, program, gl) {
-    this.name = name
-    this.suffix = suffix
-    this.gl = gl
-    this.program = program
-    this.location = gl.getUniformLocation(program, name)
+    this.name = name;
+    this.suffix = suffix;
+    this.gl = gl;
+    this.program = program;
+    this.location = gl.getUniformLocation(program, name);
   }
 
   set(...values) {
-    let method = 'uniform' + this.suffix
-    let args = [this.location].concat(values)
-    this.gl[method].apply(this.gl, args)
+    let method = "uniform" + this.suffix;
+    let args = [this.location].concat(values);
+    this.gl[method].apply(this.gl, args);
+  }
+}
+
+export class EasingUniform {
+  name: any;
+  suffix: any;
+  gl: any;
+  program: any;
+  location: any;
+  state: any;
+  lastCallback: any;
+
+  constructor(name, suffix, program, gl) {
+    this.name = name;
+    this.suffix = suffix;
+    this.gl = gl;
+    this.program = program;
+    this.location = gl.getUniformLocation(program, name);
+    this.state = {};
+  }
+
+  set(value, opts: { delay?: number } = {}) {
+    if (this.lastCallback) this.lastCallback.kill();
+    this.lastCallback = gsap.to(this, 1, {
+      state: value,
+      ease: Elastic.easeOut,
+      onUpdate: () => {
+        this.gl["uniform" + this.suffix].apply(
+          this.gl,
+          [this.location].concat(this.state)
+        );
+      },
+      ...opts
+    });
   }
 }
 
 export class Rect {
-  verts: Float32Array
+  verts: Float32Array;
 
   constructor(gl: any) {
-    this.verts = new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1])
-    var buffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-    gl.bufferData(gl.ARRAY_BUFFER, this.verts, gl.STATIC_DRAW)
+    this.verts = new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1]);
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, this.verts, gl.STATIC_DRAW);
   }
 
   render(gl: any) {
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
   }
 }
